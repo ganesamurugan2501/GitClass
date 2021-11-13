173ee6fd532f3cdfb208e5a2500980e990a6b263
ganesamurugan2501 <ganeshm2501@gmail.com>
2021-11-13 12:34:27 +0530

    text

---
+++ src/test/java/org/hexa/GitClass.java
@@ -0,0 +1,17 @@
+package org.hexa;
+
+public class GitClass {
+private void method1() {
+	// TODO Auto-generated method stub
+
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
+
+}
